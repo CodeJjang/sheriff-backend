436e97fa5540b4ff142d1698ef80741677a139c9
OzAdari <ozzadari@gmail.com>
2017-08-24 15:51:52 +0300

    Implemented alpr

--- SherrifBackend/Controllers/ValuesController.cs
+++ SherrifBackend/Controllers/ValuesController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using OpenAlprApi.Api;
+using OpenAlprApi.Client;
+using OpenAlprApi.Model;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -18,7 +21,20 @@ namespace SherrifBackend.Controllers
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            var apiInstance = new DefaultApi();
+            var imageBytes = Convert.ToBase64String(System.IO.File.ReadAllBytes(@"D:\Downloads\Junk\carphotos\20170606_143120.jpg"));
+            var secretKey = "sk_68915d22026ad9d2e1d979ae";
+            var country = "eu";
+            var recognizeVehicle = 1;
+            try
+            {
+                InlineResponse200 result = apiInstance.RecognizeBytes(imageBytes, secretKey, country, recognizeVehicle);
+                return result.ToJson();
+            }
+            catch(Exception c)
+            {
+                return c.ToString();
+            }
         }
 
         // POST api/values
