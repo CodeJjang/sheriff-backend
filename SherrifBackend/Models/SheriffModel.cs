fe051b3c8f862d8668d3605fd30a88362b4dc829
Dolev <dolevco1@gmail.com>
2017-08-24 15:43:23 +0300

    start implement db

---
+++ SherrifBackend/Models/SheriffModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SherrifBackend.Models.Entities;
+using System.Threading.Tasks;
+using MongoDB.Driver.Core;
+using System.Threading;
+using System.Configuration;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using SherrifBackend.Models.Utilities;
+
+namespace SherrifBackend.Models
+{
+    public static class SheriffModel
+    {
+        private const string USERS_COLLECTION = "USERS";
+
+        public static void AddUser(User user)
+        {
+            IMongoDatabase mongoDB = ConnectionManager.GetMongoDatabase();
+            IMongoCollection<User> users = mongoDB.GetCollection<User>(USERS_COLLECTION);
+            users.InsertOne(user);
+        }
+
+        public static void SetUserAsSheriff(User user, bool isSheriff)
+        {
+            IMongoDatabase mongoDB = ConnectionManager.GetMongoDatabase();
+            IMongoCollection<User> users = mongoDB.GetCollection<User>(USERS_COLLECTION);
+            var query = MongoDB.Driver.Builders<User>.Filter.Eq("Id", user.Id);
+            var update = MongoDB.Driver.Builders<User>.Update.Set("IsSheriff", isSheriff);
+            users.UpdateOne(query, update);
+        }
+
+        public static List<Bounty> GetUserBounties(string userId)
+        {
+            IMongoDatabase mongoDB = ConnectionManager.GetMongoDatabase();
+            IMongoCollection<Bounty> users = mongoDB.GetCollection<Bounty>(USERS_COLLECTION);
+            var query = MongoDB.Driver.Builders<User>.Filter.Eq("Id", userId);
+            return null;
+
+        }
+
+        public static void InsertVehicle(Vehicle vehicle)
+        {
+
+        }
+    }
+}
---
+++ SherrifBackend/Models/Utilities/ConnectionManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using MongoDB.Driver.Core;
+using System.Threading;
+using System.Configuration;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace SherrifBackend.Models.Utilities
+{
+    public static class ConnectionManager
+    {
+        #region Data Members
+
+        private static MongoClient _mongoClient;
+
+        #endregion
+
+        #region Public Methods
+        public static MongoClient GetMongoClient(string connectionString)
+        {
+            if (_mongoClient == null)
+            {
+                _mongoClient = new MongoClient(connectionString);
+            }
+
+            return _mongoClient;
+        }
+
+        // Connect to local host default configuration
+        public static MongoClient GetMongoClient()
+        {
+            return GetMongoClient(ConfigurationManager.AppSettings["MongoHost"]);
+        }
+
+        public static IMongoDatabase GetMongoDatabase()
+        {
+            MongoClient client = ConnectionManager.GetMongoClient();
+            return client.GetDatabase(ConfigurationManager.AppSettings["MongoDatabase"]);
+        }
+
+        public static IMongoCollection<BsonDocument> GetMongoCollection(string dbName, string collectionName)
+        {
+            MongoClient client = ConnectionManager.GetMongoClient();
+            IMongoDatabase mongoDB = client.GetDatabase(dbName);
+            return mongoDB.GetCollection<BsonDocument>(collectionName);
+        }
+
+        #endregion
+    }
+}
